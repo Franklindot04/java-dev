68cd2ce173bd64cd4d1934c0699058d6d0cba19d
Ajero Franklin <244808567+Franklindot04@users.noreply.github.com>
2026-09-02 22:08:13 +0300

    Establish Java service foundation

---
+++ src/test/java/com/franklin/javadev/JavaDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.franklin.javadev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
